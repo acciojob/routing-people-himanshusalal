c2b0c8c7c2d1b174659884efeb75ba57b5ccafdc
himanshusalal <himanshusalal517@gmail.com>
2026-06-01 13:19:17 +0530

    start

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
-
 import React from "react";
-import './../styles/App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import UserList from "./UserList";
+import UserDetails from "./UserDetails";
 
-const App = () => {
+function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={UserList} />
+        <Route path="/users/:id" component={UserDetails} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
